3759de19df548210c75703db4b31358ed1eba544
Vladyslav Yefimenko <Slain505>
2024-06-20 23:56:27 +0200

    - Add nav, home, register components  - Add Account service  - Small fixes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,29 +1,27 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavComponent, HomeComponent]
 })
-export class AppComponent implements OnInit{
-  title = 'Practice Angular App';
-  users: any;
-
-  constructor(private http: HttpClient) {
-  }
+export class AppComponent implements OnInit {
+  private accountService = inject(AccountService);
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (data) => {
-        this.users = data;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-      complete: () => {
-        console.log('Complete');
-      }
-    });
-    }
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    this.accountService.currentUser.set(user);
+  }
+}
